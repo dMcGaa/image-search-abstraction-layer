19a6b2a94fddeba7a1278af4ced9f6e1e9d4937e
dmcgaa <mcgaa.david@gmail.com>
2016-01-07 18:34:30 +0000

    parse input URL done

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var searchTerm = [];
+var searchResult = [];
+var recentSearch = [];
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -11,6 +15,33 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index')
+});
+app.get('/api/imagesearch/*', function(request, response) {
+  console.log(request.url);
+  var str = request.url;
+  str = str.slice(("/api/imagesearch/").length);
+
+  if ((/\w+/).test(str)) {
+
+    if ((/\?offset=\d+\W|\&/).test(str)) {
+      var tempArr = str.match(/\?offset=\d+\W|\&/);
+      console.log(tempArr);
+      console.log("before offset remove: " + str);
+      str = str.replace(tempArr[0], tempArr[0][tempArr[0].length - 1]);
+      str = str.slice(0, str.length-1); //remove last character (should be "&")
+      console.log("after offset remove: " + str);
+    }
+    searchTerm = str.split(/\%\w{2}|\W/);
+    console.log(searchTerm);
+    //request.url = "test%20test";  //cannot reset request url in address bar ("beauitify" it)
+
+    response.send(searchTerm);
+  }
+  else {
+    console.log(str);
+    console.log("Invalid Input");
+    response.send(searchTerm);
+  }
 });
 
 app.listen(app.get('port'), function() {
