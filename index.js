f293cc5acf67c4adb2f2276afba4d5f309392f20
dmcgaa <mcgaa.david@gmail.com>
2016-10-31 04:48:37 +0000

    use imageSearch library, send response when 'latest' url is pinged

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 var express = require('express');
+var imageSearch = require('./image-search-layer/util-image-search');
 var app = express();
 
-var searchTerm = [];
-var searchResult = [];
 var recentSearch = [];
 
 app.set('port', (process.env.PORT || 5000));
@@ -17,32 +16,20 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 app.get('/api/imagesearch/*', function(request, response) {
-  console.log(request.url);
   var str = request.url;
   str = str.slice(("/api/imagesearch/").length);
+  var searchObj = imageSearch.imageSearch(str);
+  response.send(searchObj.searchTerms);
+  
+  //add to recently searched array
+  recentSearch.push({
+    term: searchObj.searchTerms.join(" "),
+    when: Date.now()
+  });
+});
 
-  if ((/\w+/).test(str)) {
-
-    if ((/\?offset=\d+\W|\&/).test(str)) {
-      var tempArr = str.match(/\?offset=\d+\W|\&/);
-      console.log(tempArr);
-      console.log("before offset remove: " + str);
-      str = str.replace(tempArr[0], tempArr[0][tempArr[0].length - 1]);
-      str = str.slice(0, str.length-1); //remove last character (should be "&")
-      console.log("after offset remove: " + str);
-    }
-    searchTerm = str.split(/\%\w{2}|\W/);
-    console.log(searchTerm);
-    //request.url = "test%20test";  //cannot reset request url in address bar ("beauitify" it)
-
-    // response.send(searchTerm);
-    response.render('pages/gapi');
-  }
-  else {
-    console.log(str);
-    console.log("Invalid Input");
-    response.send(searchTerm);
-  }
+app.get('/api/latest/imagesearch/', function(request, response) {
+  response.send(recentSearch);
 });
 
 app.listen(app.get('port'), function() {
